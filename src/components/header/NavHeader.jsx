c4df693daca3ef3bb52fa0c5417dbee6f5fe176d
Aron Braggans <aronbraggans@Eleanor-Rigby.localdomain>
2020-06-17 22:34:58 -0500

    menu changes

--- src/components/header/NavHeader.jsx
+++ src/components/header/NavHeader.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   AppBar,
   Toolbar,
@@ -11,6 +12,8 @@ import {
 import { AccountCircle } from '@material-ui/icons';
 import { NavLink } from 'react-router-dom';
 import { blue, red } from '@material-ui/core/colors';
+import { logout } from './../../actions/auth';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -28,28 +31,75 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function NavHeader(props) {
-  const menuId = '';
-  function handleProfileMenuOpen() {}
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem
+        onClick={() => {
+          dispatch(logout());
+          handleMenuClose();
+        }}
+      >
+        Logout
+      </MenuItem>
+    </Menu>
+  );
+
   return (
-    <AppBar position="absolute" className={classes.appBar}>
-      <Toolbar variant="dense">
-        <Typography variant="h6" componet={NavLink} className={classes.title}>
-          Trails Admin
-        </Typography>
-        <div className={classes.sectionDesktop}>
-          <IconButton
-            edge="end"
-            aria-label="account of current user"
-            aria-controls={menuId}
-            aria-haspopup="true"
-            onClick={handleProfileMenuOpen}
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-        </div>
-      </Toolbar>
-    </AppBar>
+    <div className={classes.grow}>
+      <AppBar position="absolute" className={classes.appBar}>
+        <Toolbar variant="dense">
+          <Typography variant="h6" componet={NavLink} className={classes.title}>
+            Trails Admin
+          </Typography>
+          <div className={classes.sectionDesktop}>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+
+      {renderMenu}
+    </div>
   );
 }
