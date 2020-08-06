0e8a3d80f81d7be37aa56676afa6e89a2d787d74
Aron Braggans <aronbraggans@Eleanor-Rigby.localdomain>
2020-08-06 18:49:48 -0500

    removed link

--- src/pages/login/forgot.js
+++ src/pages/login/forgot.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import Container from '../../molecules/container';
 import Column from '../../molecules/column';
